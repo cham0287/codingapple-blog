234ecee438fa99c1dc5278b90d0de18d27327c48
cham0287 <62415003+cham0287@users.noreply.github.com>
2022-07-03 23:50:11 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ function App() {
     '강남 우동맛집',
     '파이썬 독학',
   ]);
+  let [따봉수, 따봉수변경] = useState([1, 2, 3]);
 
+  function addLike(i) {
+    let new따봉수 = [...따봉수];
+    new따봉수[i] += 1;
+    따봉수변경(new따봉수);
+  }
   return (
     <div className='App'>
       <div className='black-nav'>
@@ -16,11 +22,18 @@ function App() {
       {글제목.map((글제목, i) => {
         return (
           <div className='list'>
-            <h4>{글제목}</h4>
+            <h4>
+              {글제목}
+              <span onClick={addLike(i)}>👍</span>
+              {따봉수[i]}
+            </h4>
             <p>7월 2일 발행</p>
           </div>
         );
       })}
+      <div>
+        <button>성별변경</button>
+      </div>
     </div>
   );
 }
